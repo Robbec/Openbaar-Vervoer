b551a741a155a51607796c0b4840af6bb0deab34
Niels Van Belle <niels.vanbelle@hotmail.com>
2017-11-30 18:12:32 +0100

    random color background

---
+++ scripts/randomColor.gd
@@ -0,0 +1,12 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	randomize();
+	var r = rand_range(0, 1);
+	var g = rand_range(0, 1);
+	var b = rand_range(0, 1);
+	set_modulate(Color(r, g, b, 0.5));
