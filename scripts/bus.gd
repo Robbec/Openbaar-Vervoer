9abb5df106f3eb2669be820dfed9765007e53797
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-01 14:36:25 +0100

    Adding shaking animation to busses

--- scripts/bus.gd
+++ scripts/bus.gd
@@ -1,16 +1,17 @@
 extends Area2D
 
+onready var animation = get_node("AnimationPlayer")
 signal busClicked
 
 func _ready():
 	connect("area_enter",self,"_on_area_enter")
 	
 func _input_event(viewport, event, shape_idx):
-	print("Input event registered.")
 	if event.type == InputEvent.MOUSE_BUTTON \
 	and event.button_index == BUTTON_LEFT \
 	and event.pressed:
 		emit_signal("busClicked")
+		animation.stop()
 		print("Bus is clicked.")
 
 func _on_area_enter(area):
