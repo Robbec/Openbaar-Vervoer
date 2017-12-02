c0bdb8f476220089ae73a154bd7881c9f79de229
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-02 22:33:55 +0100

    Added level10 scene

--- scripts/highscoreLabel.gd
+++ scripts/highscoreLabel.gd
@@ -1,10 +1,5 @@
 extends Label
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	if(global._is_new_highscore()):
-		set_text("NEW HIGHSCORE!!")
-	pass
+		set_text("NEW HIGHSCORE!")
