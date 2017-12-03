79b2494117d0ea3c6f7d3693f56e2aad0d53aaaa
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-03 16:21:56 +0100

    Revert "Revert "Merge branch 'Jeroen'""
    Trying to merge the screenWin scene correctly.

--- scenes/screen/screenWin.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite
-
-onready var timerLabel = get_node("winText/timerLabel")
-
-func _ready():
-	print(timerLabel)
-	timerLabel.time = global.localscore
--- scripts/trophy.gd
+++ scripts/trophy.gd
@@ -4,11 +4,12 @@ var gold = preload("res://textures/3 sterren.png")
 var silver = preload("res://textures/2 sterren.png")
 var brons = preload("res://textures/1 sterren.png")
 var nothing = preload("res://textures/0 sterren.png")
+onready	var star1 = global._get_star_value(1,global.level) 
+onready var star2 = global._get_star_value(2,global.level)
+onready	var star3 = global._get_star_value(3,global.level)
 
 func _ready():
-	var star2 = global._get_star_value(2,global.level)
-	var star3 = global._get_star_value(3,global.level)
-	var star1 = global._get_star_value(1,global.level) 
+	sound._play_sound("win")
 	if(global.localscore < star3):
 		set_texture(gold)
 	elif(global.localscore < star2):
