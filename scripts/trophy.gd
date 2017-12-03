21c0db8641331aef27762f02d4a0d5cbce4ccc33
sebaztiano <sebbe.20@hotmail.com>
2017-12-03 15:09:01 +0100

    removed stars on winscreen and used global.starscore in trophy

--- scripts/trophy.gd
+++ scripts/trophy.gd
@@ -2,9 +2,18 @@ extends Sprite
 
 var gold = preload("res://textures/3 sterren.png")
 var silver = preload("res://textures/2 sterren.png")
+var brons = preload("res://textures/1 sterren.png")
+var nothing = preload("res://textures/0 sterren.png")
 
 func _ready():
-	if(global.localscore < 6):
-		set_texture(silver)
-	if(global.localscore < 4):
+	var star2 = global._get_star_value(2,global.level)
+	var star3 = global._get_star_value(3,global.level)
+	var star1 = global._get_star_value(1,global.level) 
+	if(global.localscore < star3):
 		set_texture(gold)
+	elif(global.localscore < star2):
+		set_texture(silver)
+	elif(global.localscore < star1):
+		set_texture(brons)
+	else:
+		set_texture(nothing)
