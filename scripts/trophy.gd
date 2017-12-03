678d3c52e610aecec78120bf6cae0c1833562305
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-03 16:33:15 +0100

    Cleaned winScreen scene
    Scene is working again. Playing win sound in script.

--- scripts/trophy.gd
+++ scripts/trophy.gd
@@ -4,9 +4,9 @@ var gold = preload("res://textures/3 sterren.png")
 var silver = preload("res://textures/2 sterren.png")
 var brons = preload("res://textures/1 sterren.png")
 var nothing = preload("res://textures/0 sterren.png")
-onready	var star1 = global._get_star_value(1,global.level) 
+onready var star1 = global._get_star_value(1,global.level) 
 onready var star2 = global._get_star_value(2,global.level)
-onready	var star3 = global._get_star_value(3,global.level)
+onready var star3 = global._get_star_value(3,global.level)
 
 func _ready():
 	sound._play_sound("win")
