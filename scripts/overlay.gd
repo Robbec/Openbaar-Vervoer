f7844256fe2ef36acb71aff7bc92b9f67ec205ee
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-03 02:56:02 +0100

    Corrected wrong names for nodes

--- scripts/overlay.gd
+++ scripts/overlay.gd
@@ -1,7 +1,7 @@
 extends Sprite
 
 func _ready():
-	get_tree().get_root().get_node("main/busContainer/busPath/busPathFollow/bus").connect("busClicked",self,"_overlay_dissapear")
+	get_tree().get_root().get_node("level/busContainer/busPath/busPathFollow/bus").connect("busClicked",self,"_overlay_dissapear")
 	set_opacity(0.8)
 	
 func _overlay_dissapear():
