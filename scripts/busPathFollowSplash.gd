52cb38e1eee3620c3a2b42763ab76fa341f5aaa2
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-04 21:13:37 +0100

    Improved level10

--- scripts/busPathFollowSplash.gd
+++ scripts/busPathFollowSplash.gd
@@ -1,14 +1,15 @@
 extends PathFollow2D
 
 export var speed = 400
+onready var filename = get_tree().get_current_scene().get_name()
 
 func _ready():
-	if (get_tree().get_current_scene().get_name() != "screenSplash"):
-		sound._play_sound("busDriving")
 	set_process(true)
 	
 func _process(delta):
 	set_offset(get_offset() + speed * delta)
-	if get_unit_offset() > 0.99:
-		get_tree().change_scene("res://scenes/mainMenu/mainMenu.tscn")
-		queue_free()
+	if (get_unit_offset() > 0.99):
+		if (filename == "screenSplash"):
+			get_tree().change_scene("res://scenes/mainMenu/mainMenu.tscn")
+		else:
+			set_offset(0)
--- scripts/versionBus.gd
+++ scripts/versionBus.gd
@@ -1,12 +1,9 @@
 extends Sprite
 var bus = preload("res://textures/freebus.png")
 var noBus = preload("res://textures/noBus.png")
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
 func _ready():
 	if (global.theme):
-		set_texture(bus);
+		set_texture(bus)
 	else:
-		set_texture(noBus);
+		set_texture(noBus)
