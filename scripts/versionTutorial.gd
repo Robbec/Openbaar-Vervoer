a27e1d9c6dae8d2528cfd353e50cd7130179311b
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-05 00:04:23 +0100

    Easier code for tutorial

--- scripts/versionTutorial.gd
+++ scripts/versionTutorial.gd
@@ -1,13 +1,5 @@
 extends Label
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	var bus;
-	if (global.theme):
-		bus = "bus"
-	else:
-		bus = "yellow rectangle"
-	set_text("Safely lead the " + bus + " to the other side of the screen. \n\n Press the " + bus + " once to start.")
+	if (!global.theme):
+		set_text(get_text().replace("bus","yellow rectangle"))
