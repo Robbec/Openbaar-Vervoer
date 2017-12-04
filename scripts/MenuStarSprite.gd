23208517ed7c044d32b6b8033fe5dfd82210a605
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-04 01:29:17 +0100

    Removed remaining relation to star1value and moving sound button

--- scripts/MenuStarSprite.gd
+++ scripts/MenuStarSprite.gd
@@ -4,16 +4,11 @@ var star1 = preload("res://textures/1 sterren.png")
 var star2 = preload("res://textures/2 sterren.png")
 var star3 = preload("res://textures/3 sterren.png")
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	var level = int(get_parent().get_text())
-	var star1value = global._get_star_value(1,level)
 	var star2value = global._get_star_value(2,level)
 	var star3value = global._get_star_value(3,level)
-	if(global._get_score(level) == 0 || global._get_score(level) > star1value):
+	if(global._get_score(level) == 0):
 		set_texture(star0)
 	elif(global._get_score(level) < star3value):
 		set_texture(star3)
@@ -21,7 +16,3 @@ func _ready():
 		set_texture(star2)
 	else:
 		set_texture(star1)
-	pass
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
