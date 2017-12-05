1a6d5ee3f65f53822d03248ab016e3985f712180
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-05 10:47:25 +0100

    Solved bug

--- scripts/busPath.gd
+++ scripts/busPath.gd
@@ -1,17 +1,15 @@
 extends Path2D
 
 var points
-var colors = [Color(.5,.5,.5),Color("fcbb00"),Color("f32c08")]
+var colors = [Color(.5,.5,.5),Color("fcbb00"),Color("f32c08"),Color(.5,.5,.5)]
 var colorIndex = 0
 
 func _ready():
-	
 	var children = get_parent().get_children()
 	for i in range(children.size()):
 		if children[i] == self:
 			colorIndex = i
-	pass
-	
+
 func _draw():
 	get_curve().set_bake_interval(50)
 	points = get_curve().get_baked_points()
