24f96facca7a4a2843577c37ca56ef9c031aeaab
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-08 16:48:20 +0100

    Corrected typo

--- scripts/LevelButton.gd
+++ scripts/LevelButton.gd
@@ -6,7 +6,7 @@ func _ready():
 	var score = global._get_score(level)
 	if (level == 1 || score == 0 && level != 1 && global._get_score(level - 1) != 0):
 		set_disabled(false)
-	elif (score == 0):
+	if (score == 0):
 		get_node("timerLabel").set_text("-:--")
 	else:
 		get_node("timerLabel").set_text(str("%.2f" % score).replace(".",":"))
