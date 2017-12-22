15f2383e39070fcf11eba38fba1bdb1f59320f14
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-22 17:47:51 +0100

    Small improvements for new users chart

--- userStudy/newUsersBarChart.R
+++ userStudy/newUsersBarChart.R
@@ -1,7 +1,8 @@
+library(dplyr)
 database <- read.csv("database.csv")
 
 # alle datums tussen 06-12 en 22-12 als characters omdat merge anders niet werkt met vreemde dateformats
-dates <- data.frame(Var1 =  as.character(strptime(seq(as.Date("2017-12-06"), as.Date("2017-12-22"), "days"), "%Y-%m-%d")))
+dates <- data.frame(Var1 =  as.character(strptime(seq(as.Date("2017-12-06"), as.Date("2017-12-20"), "days"), "%Y-%m-%d")))
 
 # filter eerste spelletje per user
 groupByUserMinTimestamp <- database %>% 
@@ -23,7 +24,7 @@ joinNewNbOfUsersPerDayWithDates <- left_join(dates, nbNewUsersPerDay, by = "Var1
 joinNewNbOfUsersPerDayWithDates$Freq[is.na(joinNewNbOfUsersPerDayWithDates$Freq)] <- 0
 
 barplot(joinNewNbOfUsersPerDayWithDates$Freq, 
-        main="New users",
+        main="New users per day",
         names.arg= format(as.Date(joinNewNbOfUsersPerDayWithDates$Var1), "%d"),
         xlab="Day of December 2017",
         ylim=c(0, 30),
