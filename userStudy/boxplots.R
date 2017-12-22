202ef669ece0dc4ca1a121a73f7d01c80fb4e7fb
Jeroen <33607235+Jeroen94@users.noreply.github.com>
2017-12-22 18:50:47 +0100

    Adapting boxplots
    Use start times

--- userStudy/boxplots.R
+++ userStudy/boxplots.R
@@ -4,14 +4,18 @@ noThemeData <- database[database$theme == 0 & database$won == 1, ]
 
 par(mfrow = c(2, 1)) # definieer een layout met 1 rij en 2 cols
 par(mar = c(4, 4, 4, 4)) # definieer de breedte van de marges
-lmts <- range(0,25)
-boxplot(startTime+time ~ level,
+lmts <- range(0,17)
+boxplot(startTime ~ level,
         data = themeData,
         outline = FALSE,
         main = "Theme",
-        ylim=lmts)
-boxplot(startTime+time ~ level,
+        ylim=lmts,
+        xlab = "Level",
+        ylab = "Starting time")
+boxplot(startTime ~ level,
         data = noThemeData,
         outline = FALSE,
         main = "No theme",
-        ylim=lmts)
+        ylim=lmts,
+        xlab = "Level",
+        ylab = "Starting time")
