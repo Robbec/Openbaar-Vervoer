4493f98de7b2f870a921f6eca2c5d5c6d5f38124
Niels Van Belle <niels.vanbelle@hotmail.com>
2017-12-22 16:14:04 +0100

    grouped barplot new users

---
+++ userStudy/replayersBarChart.R
@@ -0,0 +1,46 @@
+database <- read.csv("database.csv")
+
+# alle datums tussen 06-12 en 22-12 als characters omdat merge anders niet werkt met vreemde dateformats
+dates <- data.frame(Var1 =  as.character(strptime(seq(as.Date("2017-12-06"), as.Date("2017-12-22"), "days"), "%Y-%m-%d")))
+
+# filter eerste spelletje per user
+groupByUserMinTimestamp <- database %>% 
+  group_by(user) %>% 
+  slice(which.min(timeStamp))
+
+# datum kolom zonder uren (day)
+controlNewUserDaysTimestamps <- cbind(groupByUserMinTimestamp, day = format(strptime(groupByUserMinTimestamp$timeStamp, "%Y-%m-%d"), "%Y-%m-%d"))
+
+# theme en no theme split
+themeControlNewUserDaysTimestamps <- controlNewUserDaysTimestamps[controlNewUserDaysTimestamps$theme == 1,]
+noThemeControlNewUserDaysTimestamps <- controlNewUserDaysTimestamps[controlNewUserDaysTimestamps$theme == 0,]
+
+# enkel user en day kolom
+themeNewUserDays <- subset(themeControlNewUserDaysTimestamps, select = c(user, day))
+noThemeNewUserDays <- subset(noThemeControlNewUserDaysTimestamps, select = c(user, day))
+
+# tel users per dag
+themeNbNewUsersPerDay <- data.frame(table(themeNewUserDays$day))
+noThemeNbNewUsersPerDay <- data.frame(table(noThemeNewUserDays$day))
+
+# merge aantal users per dag met alle datums
+themeJoinNewNbOfUsersPerDayWithDates <- left_join(dates, themeNbNewUsersPerDay, by = "Var1")
+noThemeJoinNewNbOfUsersPerDayWithDates <- left_join(dates, noThemeNbNewUsersPerDay, by = "Var1")
+
+# vervang NA door 0
+themeJoinNewNbOfUsersPerDayWithDates$Freq[is.na(themeJoinNewNbOfUsersPerDayWithDates$Freq)] <- 0
+noThemeJoinNewNbOfUsersPerDayWithDates$Freq[is.na(noThemeJoinNewNbOfUsersPerDayWithDates$Freq)] <- 0
+
+# combine theme en no theme new users
+bothNewUsers <- data.frame(cbind(themeJoinNewNbOfUsersPerDayWithDates$Freq, noThemeJoinNewNbOfUsersPerDayWithDates$Freq))
+matrixNewUsers <- do.call(rbind, bothNewUsers)
+
+# grouped barplot new users
+barplot(matrixNewUsers, 
+        main="New users",
+        beside = TRUE,
+        names.arg= format(as.Date(themeJoinNewNbOfUsersPerDayWithDates$Var1), "%d"),
+        xlab="Day of December 2017",
+        ylim=c(0, 20),
+        ylab="Number of users",
+        las=2)
